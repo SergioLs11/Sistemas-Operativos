239d6f961e48e9d063f6418d3d5af0fbb4018d3e
Sergio López <140862209+SergioLs11@users.noreply.github.com>
2024-10-01 12:49:19 -0500

    Update and rename SERVIDOR-CLIENTE to SERVIDOR-CLIENTE.c

--- Pipes/Bidireccional/SERVIDOR-CLIENTE.c
+++ Pipes/Bidireccional/SERVIDOR-CLIENTE.c
@@ -14,19 +14,22 @@ Se creará un sistema clinte servidor, en el cual se va a poder realizar una com
 #include <unistd.h>
 #include <string.h>
 
+//Se define el archivo tipo named PIPE
 #define FIFO_FILE "/tmp/fifo_twoway"
 void de_cabeza(char *);
 int main() {
    int fd;
-   char contenedor[80]; //contenedor
-   char fin[10]; // fin
-   int finalizar; //finalizar
-   int lectura; //lectura
+   char contenedor[80]; 
+   char fin[10]; 
+   int finalizar; 
+   int lectura; 
  
-   /* Create the FIFO if it does not exist */
+   //Se crea el archivo FIFO si no se ha creado aún
    mkfifo(FIFO_FILE, S_IFIFO|0660);
    strcpy(fin, "fin");
    fd = open(FIFO_FILE, O_RDWR);
+
+   //Se inicia el ciclo para la recepción del mensaje y reenvío de la palabra invertida
    while(1) {
       lectura = read(fd, contenedor, sizeof(contenedor));
       contenedor[lectura] = '\0';
@@ -50,7 +53,7 @@ int main() {
  }
 
 
-
+//Función encargada de invertir la palabra recibida
 void de_cabeza(char *str) {
    int ultimo, limite, primero;
    char temp;
